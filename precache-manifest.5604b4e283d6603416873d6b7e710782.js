0f428756750f11e92eef7a0c3936ee941ca3fb83
谢乾 <xieqian0928@qq.com>
2020-12-31 15:10:19 +0800

    deploy

---
+++ precache-manifest.5604b4e283d6603416873d6b7e710782.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8ec5a9a7854c56d98f4",
+    "url": "/vue-blog/css/app.17d0086d.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-blog/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "482d1ec7e41964b23cac830d4c5e8ed3",
+    "url": "/vue-blog/index.html"
+  },
+  {
+    "revision": "89124b58b963d4bdc5a4",
+    "url": "/vue-blog/js/about.2b18ad61.js"
+  },
+  {
+    "revision": "e8ec5a9a7854c56d98f4",
+    "url": "/vue-blog/js/app.9a735894.js"
+  },
+  {
+    "revision": "942a841bdc26234fe999",
+    "url": "/vue-blog/js/chunk-vendors.8380736d.js"
+  },
+  {
+    "revision": "bc9a2d24b88e57f80dc8949bab6da922",
+    "url": "/vue-blog/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-blog/robots.txt"
+  }
+]);
